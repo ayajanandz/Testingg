32dbc3a5487a4da22c28b5d040ca39d0584f8148
Ayaj Anand <sam.ayajanand@gmail.com>
2021-12-27 13:49:24 +0530

    test commit

---
+++ app/src/main/java/com/example/henloo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.henloo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        Log.i("Info", "Button Clicked")
+
+    }
+}
